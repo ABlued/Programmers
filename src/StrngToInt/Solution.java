b77a886d39782da8f77115411da277cb958e1957
ablued <skwodhkswkd@naver.com>
2021-01-10 16:07:15 +0900

    문자열을 정수로 바꾸기 문제 해결

---
+++ src/StrngToInt/Solution.java
@@ -0,0 +1,13 @@
+package StrngToInt;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String input = scan.nextLine();
+		int output = Integer.parseInt(input);
+		System.out.println(output);
+	}
+}
